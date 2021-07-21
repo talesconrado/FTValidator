2ea94d076fc66dd2ca11c6ea09ab5047d66ecbd6
Tales Conrado <talesmateuscc@gmail.com>
2021-07-21 09:47:39 -0300

    Initial Commit

---
+++ FTValidatorKit/FTValidatorKit.h
@@ -0,0 +1,18 @@
+//
+//  FTValidatorKit.h
+//  FTValidatorKit
+//
+//  Created by Tales Conrado on 21/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FTValidatorKit.
+FOUNDATION_EXPORT double FTValidatorKitVersionNumber;
+
+//! Project version string for FTValidatorKit.
+FOUNDATION_EXPORT const unsigned char FTValidatorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTValidatorKit/PublicHeader.h>
+
+
